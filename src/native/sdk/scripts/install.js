02b5b5569c512254dba97ca20df7c5c2b612a07b
HaseenaSainul <h.sainul@metrological.com>
2022-12-22 04:33:23 -0800

    install script: function updates

--- src/native/sdk/scripts/install.js
+++ src/native/sdk/scripts/install.js
@@ -1,26 +1,27 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+const readdir = require('fs/promises');
 const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
 
 var dstPath = process.env.NODE_INSTALL_PATH;
-var name = process.env.NATIVE_LIB_NAME;
+var fireboltTarget = process.env.NATIVE_LIB_NAME;
 var versionStr = process.env.NATIVE_LIB_VERSION;
-var version = versionStr.split(".");
 
-var libName = 'lib' + name + '.so.' + version[0] + '.' + version[1] + '.' + version[2];
-var linkName = 'lib' + name + '.so.' + version[0];
-
-var srcPath = 'src/native/sdk/build/'
+var buildPath = 'src/native/sdk/build/';
 var buildType = 'Release/';
-createDir(dstPath + '/usr/lib/pkgconfig');
-createDir(dstPath + '/etc');
+var includePath = 'src/native/sdk/Source';
 
-copyFile(srcPath + 'Source/' + 'FireboltSDK.pc', dstPath + '/usr/lib/pkgconfig/FireboltSDK.pc');
-copyFile(srcPath + 'Source/' + 'config/FireboltSDK.json', dstPath + '/etc/FireboltSDK.json');
-copyFile(srcPath + buildType + libName, dstPath + '/usr/lib/' + libName);
+// Install config files
+installFiles(buildPath + 'Source/', dstPath + '/usr/lib/pkgconfig/', ".pc");
+installFiles(buildPath + 'Source/', dstPath + '/usr/lib/cmake/' + fireboltTarget, ".cmake", "Config");
+installFiles(buildPath + 'Source/CMakeFiles/Export/lib/cmake/' + fireboltTarget, dstPath + '/usr/lib/cmake/' + fireboltTarget, ".cmake", "Targets");
+installFiles(buildPath + 'Source/config', dstPath + '/etc/', ".json");
 
-createFileLink(libName, dstPath + '/usr/lib/' + linkName);
-createFileLink(linkName, dstPath + '/usr/lib/libFireboltSDK.so');
+//installFiles(includePath + 
+
+installLibraries(buildPath + buildType, dstPath + '/usr/lib/', versionStr);
 
 function copyFile(src, dst) {
     fs.copyFileSync(src, dst);
@@ -39,5 +40,87 @@ function createDir(dir) {
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir, { recursive: true });
         console.log('Directory ' + dir + ' is created.')
+    }
+}
+
+function copyFiles(src, dst) {
+  fse.copySync(src, dst, { overwrite: true|false })
+  console.log(src + ' is copied to ' + dst);
+}
+
+function getExtension(fileName) {
+    return fileName.substring(fileName.indexOf('.')); 
+}
+
+function getName(fileName) {
+    var	fileNameParts = fileName.split(".");
+    return fileNameParts[0];
+}
+
+function installFiles(src, dst, ext, substring) {
+    const files = fs.readdirSync(src);
+
+    if (files.length > 0) {
+        createDir(dst);
+    }
+
+    console.log('Install Files fileName = ' + files);
+    for (const file of files) {
+        const fileExt = getExtension(file);
+        console.log('fileName = ' + file);
+        console.log('fileExt = ' + fileExt);
+
+        if (typeof ext !== 'undefined') {
+            console.log('extension = ' + ext);
+            if (fileExt === `${ext}`) {
+                const fileName = path.basename(file);
+                if (typeof substring !== 'undefined') {
+                    if (fileName.includes(substring)) {
+                        console.log('matched fileName = ' + file);
+                        copyFile(src + '/' + file, dst + '/' + file);
+                    }
+                } else {
+                    console.log('matched fileName = ' + file);
+                    copyFile(src + '/' + file, dst + '/' + file);
+		}
+            }
+        } else {
+            console.log('installed fileName = ' + file);
+            copyFile(src + '/' + file, dst + '/' + file);
+	}
+    }
+}
+
+function installLibraries(src, dst, versionString) {
+   if (typeof versionString !== 'undefined') {
+       var version = versionString.split(".");
+       var libExtension = ".so." + versionString;
+       console.log('calling extension = ' + libExtension);
+       installFiles(src, dst, libExtension);
+       var linkExtension = ".so." + version[0];
+       console.log('calling extension = ' + libExtension);
+       createFilesLinkByExtension(src, dst, libExtension, linkExtension);
+       libExtension = linkExtension;
+       linkExtension = ".so";
+       console.log('calling extension = ' + libExtension);
+       createFilesLinkByExtension(src, dst, libExtension, linkExtension);
+   } else {
+       installFiles(src, dst, "so");
+   }
+}
+
+function createFilesLinkByExtension(src, dst, srcExtension, linkExtension)
+{
+    const files = fs.readdirSync(src);
+
+    for (const file of files) {
+        const fileExt = getExtension(file);
+        console.log('fileName = ' + file);
+
+        if (fileExt === `${srcExtension}`) {
+  	    const fileName = path.basename(file);
+            console.log('file name inside createFilesLinkByExtension = ' + getName(fileName));
+            createFileLink(fileName, dst + '/' + getName(fileName) + linkExtension);
+        }
     }
 }
