cbf1bbf48de41769176b5d3e3ae17b45d763a864
HaseenaSainul <h.sainul@metrological.com>
2023-05-05 01:15:05 -0700

    Remove hardcoded thunder/sysroot path

--- src/native-c/build-openrpc.sh
+++ src/native-c/build-openrpc.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf build
-cmake -B./build -S. -DSYSROOT_PATH="/home/haseenasainul/firebolt-r2/firebolt-openrpc/firebolt" -DENABLE_TESTS="ON"
+cmake -B./build -S. -DSYSROOT_PATH=${SYSROOT_PATH} -DENABLE_TESTS="ON"
 cmake --build ./build/
